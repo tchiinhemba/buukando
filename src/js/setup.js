031b390ca3cd86b02ad7a2c634886511e49e3d23
Tchiinhemba <tchiinhemba@outlook.com>
2020-10-05 16:05:49 +0100

    quase terminado

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,16 +1,12 @@
-$('.owl-carousel').owlCarousel({
-    loopt: false,
-    margin: 10,
-    nav: false,
-    responsive: {
-        0 : {
-            items: 1
-        },
-        600 : {
-            items: 3
-        },
-        1000 : {
-            items: 5
-        }
-    }
-})
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+
