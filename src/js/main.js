afcbb93e439a75a8b177084c25cd18460d39f75e
Tchiinhemba <tchiinhemba@outlook.com>
2020-10-06 22:42:01 +0100

    trabalhando com js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
-var pagIndex = document.querySelector('#principal');
-var gitHb = document.querySelector('#nomefooter');
 
-pagIndex.onclick = function() {
-    pagIndex.href = "sobre.html"
-};
+
+var foto = document.querySelector('#foto');
+
+
+
+// EasterEgg
+
+foto.onclick = function(){ window.open(
+    'https://drive.google.com/drive/folders/1qtxttmJeNK8ZmHSCx_UfsqmbOFu1H_y6?usp=sharing', '_blank'
+)};
+foto.addEventListener('click', function(){ window.open(
+    'https://drive.google.com/drive/folders/1qtxttmJeNK8ZmHSCx_UfsqmbOFu1H_y6?usp=sharing', '_blank'
+)}, false);
+
+
+document.removeEventListener('click', function(){
+    window.open(
+        'https://drive.google.com/drive/folders/1qtxttmJeNK8ZmHSCx_UfsqmbOFu1H_y6?usp=sharing', '_blank'
+    )
+}, true);
 
 
 
