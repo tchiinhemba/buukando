0a880fde9d810d6cfe25df9b83cd32c597d870d3
Tchiinhemba <tchiinhemba@outlook.com>
2020-10-06 02:38:30 +0100

    update detalhes finais

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+
+
+function verc() {
+    window.open(
+        'https://drive.google.com/drive/folders/1BHGZOrX2KBBdgrHl-Pa_XY6pEnm9ljxg?usp=sharing', '_blank'
+    )
+}
+function vercpp() {
+    window.open(
+        ''
+    )
+}
+
+
+function vertudo() {
+    window.open(
+        'https://drive.google.com/drive/folders/1a_1mL-ktVT-qiCxBj0ofNwRGOY88WSyf?usp=sharing', '_blank'
+    )
+}
