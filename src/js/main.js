1dac66ff1a1b3a29bfabc773d50a4acd7cab86b7
Tchiinhemba <tchiinhemba@outlook.com>
2020-10-06 09:56:20 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,10 @@ function vertudo() {
         'https://drive.google.com/drive/folders/1a_1mL-ktVT-qiCxBj0ofNwRGOY88WSyf?usp=sharing', '_blank'
     )
 }
+
+
+function easterEgg() {
+    window.open(
+        'https://drive.google.com/drive/folders/1qtxttmJeNK8ZmHSCx_UfsqmbOFu1H_y6?usp=sharing', '_blank'
+    )
+}
